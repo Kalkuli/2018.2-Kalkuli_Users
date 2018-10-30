a33bfe8c026a491faee3b87c88c87421875fd811
phe0 <pheofo@gmail.com>
2018-10-29 22:30:50 -0300

    Add success test layout

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -12,6 +12,23 @@ class TestUserService(BaseTestCase):
         self.assertIn('Welcome to Kalkuli Users Service!!', data['data'])
 
 
+    def test_users(self):
+        with self.client:
+            response = self.client.post(
+                '/add_company',
+                data = json.dumps({
+
+                }),
+                content_type = 'application/json',
+            )
+
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('success', data['status'])
+            self.assertIn('Company was created!', data['message'])
+
+
         
 if __name__ == '__main__':
     unittest.main()
