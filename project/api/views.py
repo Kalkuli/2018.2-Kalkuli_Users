15ac06b21d95f2c9abe898cf369a20185e03abc9
Lucas Dutra <ldutra98@gmail.com>
2018-11-03 02:35:06 -0300

    Create register route
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -1,10 +1,9 @@
 from flask import request, jsonify, Blueprint
-
-from sqlalchemy import exc 
+from sqlalchemy import exc, or_ 
 from project.api.models import Company
 from project.api.models import User
 
-from project import db
+from project import db, bcrypt
 
 user_blueprint = Blueprint('user', __name__)
 
@@ -60,3 +59,40 @@ def add_company_user():
             'status': 'fail',
             'message': 'company could not be saved'
         }), 400
+
+@user_blueprint.route('/auth/register', methods=['POST'])
+def register_user():
+    post_data = request.get_json()
+    response_object = {
+        'status': 'fail',
+        'message': 'Invalid payload.'
+    }
+    if not post_data:
+        return jsonify(response_object), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+    company_id = post_data.get('company_id')
+    try:
+        user = User.query.filter(
+        or_(User.username == username, User.email == email)).first()
+        if not user:
+            new_user = User(
+                username=username,
+                email=email,
+                password=password,
+                company_id=company_id
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            auth_token = new_user.encode_auth_token(new_user.id)
+            response_object['status'] = 'success'
+            response_object['message'] = 'Successfully registered.'
+            response_object['auth_token'] = auth_token.decode()
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That user already exists.'
+            return jsonify(response_object), 400
+    except (exc.IntegrityError, ValueError) as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
