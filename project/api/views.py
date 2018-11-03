afb9f7ba92e22fbea3330e1a53cf5864815bca2f
Lucas Dutra <ldutra98@gmail.com>
2018-11-03 17:31:07 -0300

    Create status auth route
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -134,3 +134,25 @@ def logout_user(resp):
         'message': 'Successfully logged out.'
     }
     return jsonify(response_object), 200
+
+@auth_blueprint.route('/auth/status', methods=['GET'])
+def get_user_status():
+# get auth token
+    auth_header = request.headers.get('Authorization')
+    response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.'
+    }
+    if auth_header:
+        auth_token = auth_header.split(' ')[1]
+        resp = User.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            user = User.query.filter_by(id=resp).first()
+            response_object['status'] = 'success'
+            response_object['message'] = 'Success.'
+            response_object['data'] = user.to_json()
+            return jsonify(response_object), 200
+        response_object['message'] = resp
+        return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 401
