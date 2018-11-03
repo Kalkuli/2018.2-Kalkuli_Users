e5f50729813cd3e4e9f982e40a2298fc88581abf
Lucas Dutra <ldutra98@gmail.com>
2018-11-02 23:57:13 -0300

    Update config with bcrypt keys
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/config.py
+++ project/config.py
@@ -3,15 +3,19 @@ import os
 class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BCRYPT_LOG_ROUNDS = 13
     
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    BCRYPT_LOG_ROUNDS = 4
 
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    BCRYPT_LOG_ROUNDS = 4
 
 class ProductionConfig(BaseConfig):
+    DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class HomologationConfig(BaseConfig):
