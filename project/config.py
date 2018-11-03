9da969a78b2702b9c3b78ea95b5fb508f4ca5d1d
Lucas Dutra <ldutra98@gmail.com>
2018-11-03 00:27:43 -0300

    Add secret key variables to config
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/config.py
+++ project/config.py
@@ -4,6 +4,9 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BCRYPT_LOG_ROUNDS = 13
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    TOKEN_EXPIRATION_DAYS = 30
+    TOKEN_EXPIRATION_SECONDS = 0
     
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
@@ -13,6 +16,8 @@ class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
     BCRYPT_LOG_ROUNDS = 4
+    TOKEN_EXPIRATION_DAYS = 0
+    TOKEN_EXPIRATION_SECONDS = 3
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
