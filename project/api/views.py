10b2d66963ba7df577a33861608fd8cae324fc1d
Lucas Dutra <ldutra98@gmail.com>
2018-11-03 02:56:16 -0300

    Create Log out route
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -2,8 +2,8 @@ from flask import request, jsonify, Blueprint
 from sqlalchemy import exc, or_ 
 from project.api.models import Company
 from project.api.models import User
-
 from project import db, bcrypt
+import time
 
 user_blueprint = Blueprint('user', __name__)
 
@@ -124,3 +124,23 @@ def login_user():
     except Exception as e:
         response_object['message'] = 'Try again.'
         return jsonify(response_object), 500
+
+@auth_blueprint.route('/auth/logout', methods=['GET'])
+def logout_user():
+    auth_header = request.headers.get('Authorization')
+    response_object = {
+        'status': 'fail',
+        'message': 'Provide a valid auth token.'
+    }
+    if auth_header:
+        auth_token = auth_header.split(' ')[1]
+        resp = User.decode_auth_token(auth_token)
+        if not isinstance(resp, str):
+            response_object['status'] = 'success'
+            response_object['message'] = 'Successfully logged out.'
+            return jsonify(response_object), 200
+        else:
+            response_object['message'] = resp
+            return jsonify(response_object), 401
+    else:
+        return jsonify(response_object), 403
