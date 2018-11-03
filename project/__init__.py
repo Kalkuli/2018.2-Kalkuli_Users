bc50514eac2a92190232bafcbf8c2b0cd5451051
Lucas Dutra <ldutra98@gmail.com>
2018-11-02 22:36:09 -0300

    Refactor init.py to fit new dependencies
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/__init__.py
+++ project/__init__.py
@@ -4,22 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 
+# instantiate the extensions
+db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+migrate = Migrate()
+bcrypt = Bcrypt()
 
-# Instantiate the app
-app	= Flask(__name__)
+def create_app(script_info=None):
+    #Instantiate the app
+    app	= Flask(__name__)
 
+    # Set Configuration
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
-# Set Configuration
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
+    # set up extensions
+    db.init_app(app)
+    toolbar.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
-# Instanciate Database
-db = SQLAlchemy(app)
+    from project.api.views import user_blueprint
+    app.register_blueprint(user_blueprint)
 
-#Set up extensions
-migrate.init_app(app, db)
-
-from project.api.views import user_blueprint
-app.register_blueprint(user_blueprint)
-
-bcrypt = Bcrypt(app)
+    return app
