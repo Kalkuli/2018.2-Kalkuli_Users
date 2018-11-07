96eb78ba8513f5d2dfc5fc57d1452fd16b40eada
Lucas Dutra <ldutra98@gmail.com>
2018-11-06 23:33:13 -0200

    Refactor registration duplicate username test
    Co-authored-by: Lucas Dutra <ldutra98@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/tests/test_auth.py
+++ project/tests/test_auth.py
@@ -68,10 +68,17 @@ class TestAuthService(BaseTestCase):
             response = self.client.post(
                 '/auth/register',
                 data=json.dumps({
+                    'company_name': 'Kalkuli',
+                    'cnpj': '00.000.000/0000-00',
+                    'company_email': 'contact.kalkuli@kalkuli.com',
+                    'fantasy_name': 'Kaliu',
+                    'cep': '00-000/00',
+                    'city': 'Brasilia',
+                    'state': 'Distrito Federal',
+                    'company_phone': '61 98888888',
                     'username': 'test',
                     'email': 'test@test.com2',
                     'password': 'test',
-                    'company_id': company.id
                 }),
                 content_type='application/json',
             )
